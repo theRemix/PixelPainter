d7883d0a143814041a47288e97df9be150939146
Jon Borgonia <jon@gomagames.com>
2014-02-06 19:20:06 -1000

    built grid

--- js/app.js
+++ js/app.js
@@ -6,8 +6,24 @@ function PixelPainter(width, height, pixel_size){
   // Array<Array<Pixel>>
   this.grid = [];
 
+  // this gets rendered
+  this.element = $('<div>');
+
+
   this.buildGrid = function () {
-    
+
+    // rows
+    for (var i = 0; i < this.width; i++) {
+      this.grid[i] = [];
+
+      // height
+      for (var k = 0; k < this.height; k++) {
+        this.grid[i][k] = new Pixel("grid", i, k);
+
+      }
+
+    }
+
   };
 
   this.buildToolbar = function () {
@@ -22,16 +38,23 @@ function PixelPainter(width, height, pixel_size){
   // returns a JQuery obj
   this.render = function () {
     
+    return this.element;
   };
 
+
+  // initialize
+  this.buildGrid();
 
 }
 
 function Pixel (id, x, y, color) {
-  this.id = id;
+  this.id = id + "_" + x + "_" + y;
   this.x = x;
   this.y = y;
   this.color = color;
+
+  this.element = $('<div>');
+  this.element.attr("id", this.id);
 
   this.changeColor = function(color){
 
@@ -91,5 +114,7 @@ ResetButton.prototype = new Button();
 
 
 $(document).ready(function () {
-  
+  var pixelPainter = new PixelPainter(5,5,15);
+  console.log(pixelPainter.grid);
+  $("#container").append(pixelPainter.render());
 });
